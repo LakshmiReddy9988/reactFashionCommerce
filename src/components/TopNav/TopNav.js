df1b5237bc4fb5b15264227d22eee9b37fb2b6d1
LakshmiReddy9988 <160628275+LakshmiReddy9988@users.noreply.github.com>
2024-03-26 09:59:36 +0000

    Cart icon alignment

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -22,26 +22,7 @@ const TopNav = function()
           <div className='brand my-1'>
             <h1 class='logo'> Coforge Rewards Shop </h1>
           </div>
-         {/*
-         
-          <div className='inp-container p-0 my-4 w-50 h-25 bg-white'>
-            <div className='dropdown m-0 p-0'>
-              <select className='select-btn w-100 p-0 m-0' >
-                <option> Men </option>
-                <option> Women </option>
-                <option> Kids </option>
-              </select>
-            </div>
-            <input className='form-control   ' placeholder='Search...'/>
-            <button> <FontAwesomeIcon icon='fa fa-search'/> </button>
-    
-          </div>
-          <div className="login-container p-0">
-          <FontAwesomeIcon icon="fa fa-user-circle"/>
-            <h5>
-              </h5>
-          </div>
-          */}
+
           <div className='cart-wishlist'>
             <ul className="p-0">
  
